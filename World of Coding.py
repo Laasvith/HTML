a8075fe4bc5b5c902ac6ba857970986d2c0dafa7
Laasvith <Itsmetag@gmail.com>
2025-09-16 18:25:45 +0930

    Add files via upload

---
+++ World of Coding.py
@@ -0,0 +1,7 @@
+#Print a Sentence
+print("Welcome to the world of Python program")
+#Print a number 
+print(25)
+print("Hello python \nThis is my 1st program")
+print("Hello", 25)
+print("Welcome to ", end="&&&")
